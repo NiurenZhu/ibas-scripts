99962f16dd52094dd095198539308e8eead1dc71
Niuren.Zhu <niuren.zhu@icloud.com>
2026-02-11 09:37:53 +0800

    更新配置

--- nginx/run_nginx_dev.sh
+++ nginx/run_nginx_dev.sh
@@ -5,31 +5,29 @@ echo '            by niuren.zhu                                                 
 echo '               2019.09.12                                                   '
 echo '  说明：                                                                     '
 echo '    1. 尝试运行Nginx容器。                                                    '
-echo '    2. 参数1，Nginx的html目录，默认当前目录下的html目录。                         '
+echo '    2. 参数1，源代码文件目录。                                                  '
 echo '****************************************************************************'
 # 设置参数变量
 WORK_FOLDER=$PWD
 NAME=ibas-nginx-dev
-PORT=15386
-MEM=128m
+PORT_V1=15386
+PORT_V2=15486
+MEM=32m
 CONFIG_FOLDER=${WORK_FOLDER}/conf.d
 
 # 参数赋值
-HTML_FOLDER=$1
-if [ "${HTML_FOLDER}" = "" ];then HTML_FOLDER=${WORK_FOLDER}/html; fi;
-
-# 检查环境
-if [ ! -e "${HTML_FOLDER}" ];then
-    echo HTML目录不存在
+CODE_FOLDER=$1
+if [ ! -e "${CODE_FOLDER}" ];then
+    echo 代码目录不存在
     exit 1
 fi;
+CODE_FOLDER=$((cd ${CODE_FOLDER} && pwd))
 
 # 显示容器信息
 echo --容器名称：${NAME}
-echo --映射端口：${PORT}
 echo --限制内存：${MEM}
-echo --数据目录：${HTML_FOLDER}
-echo --配置目录：${CONFIG_FOLDER}
+echo --映射端口：${PORT_V1} & ${PORT_V2}
+echo --代码目录：${CODE_FOLDER}
 
 # 删除已经存在
 docker rm -vf ${NAME}
@@ -37,9 +35,10 @@ docker rm -vf ${NAME}
 docker run \
    --name ${NAME} \
    -m ${MEM} \
-   -p ${PORT}:80 \
-   -v ${HTML_FOLDER}:/home/niurenzhu/Codes/ \
+   -p ${PORT_V1}:80 \
+   -p ${PORT_V2}:90 \
+   -v ${CODE_FOLDER}:${CODE_FOLDER} \
    -v ${CONFIG_FOLDER}:/etc/nginx/conf.d \
-   -d nginx:alpine
+   -d colorcoding/nginx:alpine
 # 显示创建结果
 docker ps -n 1
